9c383d6edab583f6441e5947df7a6b739c82d22a
daniel arturo casals amat <dacasals@uci.cu>
2017-01-13 09:10:09 -0500

    Update CacheExtension.php, limpiando referencias al bundle de Aspectos.

--- DependencyInjection/CacheExtension.php
+++ DependencyInjection/CacheExtension.php
@@ -6,7 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use UCI\Boson\AspectBundle\DependencyInjection\AspectExtension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -25,9 +24,5 @@ class CacheExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-
-        $AspectExt = new AspectExtension();
-        $AspectExt->loadFileAspects($container);
-
     }
 }
